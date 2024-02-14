389278ff8c46477945a30718734d5f93759898f2
JesusJr <Jesusujr101@gmail.com>
2024-02-14 18:37:51 -0500

    changed resume back

--- src/assets/Resume.jsx
+++ src/assets/Resume.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from './assets/Resume.pdf';
+import resume from '../../assets/Resume.pdf';
 
 export default function Resume() {
     const resumeFileName = 'Jesus_Uriostegui';
